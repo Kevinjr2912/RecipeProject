f731cfe77cb34e1bbf57ae8b0525d4c5f7274913
Kevin <moralesperezkevin7@gmail.com>
2024-10-30 13:09:26 -0600

    add: Creando el componente form-recipe

--- RecipeProject/src/app/recipes/recipes.module.ts
+++ RecipeProject/src/app/recipes/recipes.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormRecipeComponent } from './form-recipe/form-recipe.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormRecipeComponent
+  ],
   imports: [
     CommonModule
   ]
