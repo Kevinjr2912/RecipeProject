2fa3233766c922f03ef96d230d53aab123f51b89
Kevin <moralesperezkevin7@gmail.com>
2024-10-30 13:04:31 -0600

    add: ELiminando el componente form-create-recipe

--- RecipeProject/src/app/recipes/recipes.module.ts
+++ RecipeProject/src/app/recipes/recipes.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormCreateRecipeComponent } from './form-create-recipe/form-create-recipe.component';
-
-
 
 @NgModule({
-  declarations: [
-    FormCreateRecipeComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule
   ]
