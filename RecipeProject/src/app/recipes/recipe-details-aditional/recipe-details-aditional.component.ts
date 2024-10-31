5c693b2294ab60f52ff7dbc45930a68977291143
Kevin <moralesperezkevin7@gmail.com>
2024-10-31 09:47:03 -0600

    add: Estructura del componente para pedir la información principal de una receta

---
+++ RecipeProject/src/app/recipes/recipe-details-aditional/recipe-details-aditional.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details-aditional',
+  templateUrl: './recipe-details-aditional.component.html',
+  styleUrl: './recipe-details-aditional.component.css'
+})
+export class RecipeDetailsAditionalComponent {
+
+}
--- RecipeProject/src/app/recipes/recipes.module.ts
+++ RecipeProject/src/app/recipes/recipes.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormRecipeComponent } from './form-recipe/form-recipe.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeDetailsAditionalComponent } from './recipe-details-aditional/recipe-details-aditional.component';
 
 @NgModule({
   declarations: [
-    FormRecipeComponent
+    FormRecipeComponent,
+    RecipeDetailsComponent,
+    RecipeDetailsAditionalComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    FormRecipeComponent
   ]
 })
 export class RecipesModule { }
