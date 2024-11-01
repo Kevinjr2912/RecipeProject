7f08e4fe2dea255463dc5533c7bd80e3af75fffd
Kevin <moralesperezkevin7@gmail.com>
2024-11-01 16:43:53 -0600

    add: Implementando el OnInit en el componente ListCardsRecipesComponent para renderizar las cards

--- RecipeProject/src/app/recipes/list-cards-recipes/list-cards-recipes.component.ts
+++ RecipeProject/src/app/recipes/list-cards-recipes/list-cards-recipes.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-cards-recipes',
   templateUrl: './list-cards-recipes.component.html',
   styleUrl: './list-cards-recipes.component.css'
 })
-export class ListCardsRecipesComponent {
+export class ListCardsRecipesComponent implements OnInit{
 
+
+  ngOnInit(): void {
+      
+  }
 }
