a9fca33e9066c29f875fe3767672f49d9a3649ce
Kevin <moralesperezkevin7@gmail.com>
2024-10-31 22:27:34 -0600

    add: creando el componente content-recipe

---
+++ RecipeProject/src/app/recipes/content-recipe/content-recipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-recipe',
+  templateUrl: './content-recipe.component.html',
+  styleUrl: './content-recipe.component.css'
+})
+export class ContentRecipeComponent {
+
+}
--- RecipeProject/src/app/recipes/recipes.module.ts
+++ RecipeProject/src/app/recipes/recipes.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormRecipeComponent } from './form-recipe/form-recipe.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { RecipeDetailsAditionalComponent } from './recipe-details-aditional/recipe-details-aditional.component';
+import { ContentRecipeComponent } from './content-recipe/content-recipe.component';
 
 @NgModule({
   declarations: [
     FormRecipeComponent,
     RecipeDetailsComponent,
-    RecipeDetailsAditionalComponent
+    RecipeDetailsAditionalComponent,
+    ContentRecipeComponent
   ],
   imports: [
     CommonModule
