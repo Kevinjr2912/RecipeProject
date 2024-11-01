8b583978336bfb54ab5bc366c082abd4e943a269
Kevin <moralesperezkevin7@gmail.com>
2024-10-31 23:39:37 -0600

    add: Estructura HTMl de content-recipe y finalización de la estructura para crear una receta

--- RecipeProject/src/app/recipes/content-recipe/content-recipe.component.ts
+++ RecipeProject/src/app/recipes/content-recipe/content-recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-content-recipe',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './content-recipe.component.css'
 })
 export class ContentRecipeComponent {
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef){}
+
+  // Inputs
+  @Input() name_process: string = '';
+
+  // Methods
+  addInput() : void {
+    // Variables that we associate with DOM elements
+    const container = this.elementRef.nativeElement.querySelector('#container-inputs');
+    const divContainer = this.renderer.createElement('div');
+    const input = this.renderer.createElement('input');
+
+    // We define the type of input to render
+    this.renderer.setAttribute(input, 'type', 'text');
+    this.renderer.setAttribute(input, 'id', 'input-content');
+
+    // We create those elements 
+    this.renderer.appendChild(divContainer, input);
+    this.renderer.appendChild(container, divContainer);
+  }
+
 
 }
--- RecipeProject/src/app/recipes/form-recipe/form-recipe.component.ts
+++ RecipeProject/src/app/recipes/form-recipe/form-recipe.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './form-recipe.component.css'
 })
 export class FormRecipeComponent {
+  sendRecipe() : void {
 
+  }
 }
